69615415ddca07b5d138f2b32faa34e5dc2a7220
Erik M <perez0020@icloud.com>
2023-10-02 10:34:46 -0400

    Initial Commit

---
+++ ClassConnectApp/ClassConnectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassConnectAppApp.swift
+//  ClassConnectApp
+//
+//  Created by Erik Mejia on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ClassConnectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
